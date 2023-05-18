89f31f2a7a6b089ea30e1196bf3cce5142c08587
YK <ykkwon@ezens.net>
2023-05-18 17:29:18 +0900

    승자 결정하기, 이후 경기 중단

--- src/component/Board.js
+++ src/component/Board.js
@@ -8,8 +8,46 @@ const Board = () => {
   const [xIsNext, setXIsNext] = useState(true);
   
   
+
+  
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
+  const winner = calculateWinner(squares);
+  let status ;
+  
+  if(winner){
+    status = `Winner : ${winner}`;
+  }else {
+    status = `Next Player : ${xIsNext ? 'X' : 'O'}`;
+  }
+  
+  
   const handleClick = (i) => {
     const newSquares = squares.slice();
+    
+    // 승자가 이미 나온 경우 경기 중지(리턴), 혹은 현재 클릭한 스퀘어 안에 이미 값이 있다면 그냥 리턴
+    if(calculateWinner(newSquares) || newSquares[i]){
+      return;
+    }
+    
     newSquares[i] = xIsNext ? 'X' : 'O' ;
     setSquares(newSquares);
     setXIsNext(!xIsNext);
@@ -22,7 +60,6 @@ const Board = () => {
                    onClick={() => handleClick(i)} />
   }
   
-  const status = `Next Player : ${xIsNext ? 'X' : 'O'} ` ;
   
   return (
     <div>
