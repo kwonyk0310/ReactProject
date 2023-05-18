d641c9e56cd1b80db471837154663f103eaa2e67
YK <ykkwon@ezens.net>
2023-05-18 16:29:25 +0900

    순서 만들어서 'O' 추가하기

--- src/component/Board.js
+++ src/component/Board.js
@@ -5,20 +5,24 @@ import "./Board.css";
 const Board = () => {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const [value, setValue] = useState("");
+  const [xIsNext, setXIsNext] = useState(true);
+  
   
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
-    setSquares(newSquares)
+    newSquares[i] = xIsNext ? 'X' : 'O' ;
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+    // 위와 같은 코드이나 약간의 차이가 있다. 위처럼 하면 두 번 반복해도 한 번 동작하고, prev를 이용하면 두 번이 따로 다 동작한다.
+    // setXIsNext(prev => !prev);
   }
   
   const renderSquare = (i) => {
     return <Square value={squares[i]}
-                   onClick={() => this.handleClick(i)} />
+                   onClick={() => handleClick(i)} />
   }
-
-  const [status, setStatus] = useState("Next Player : X, O") ;
+  
+  const status = `Next Player : ${xIsNext ? 'X' : 'O'} ` ;
   
   return (
     <div>
