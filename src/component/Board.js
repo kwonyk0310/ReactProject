444386302f032461fea28d47add9ad82367bbc1b
YK <ykkwon@ezens.net>
2023-05-18 15:50:24 +0900

    class형 컴포넌트에서 함수형 컴포넌트로 변경

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,35 +1,45 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Square from "./Square";
 import "./Board.css";
 
-class Board extends Component {
-  renderSquare(i){
-    return <Square value={i}/>
+const Board = () => {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [value, setValue] = useState("");
+  
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = 'X';
+    setSquares(newSquares)
   }
   
-  render() {
-    const status = "Next Player : X, O" ;
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+  const renderSquare = (i) => {
+    return <Square value={squares[i]}
+                   onClick={() => this.handleClick(i)} />
   }
+
+  const [status, setStatus] = useState("Next Player : X, O") ;
+  
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
 export default Board;
