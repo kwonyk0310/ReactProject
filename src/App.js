ae96ff8298d609d618ba0f9babbb170abbcf91cb
YK <ykkwon@ezens.net>
2023-05-19 18:15:20 +0900

    JumpTo 함수 추가

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   
   const [history, setHistory] = useState([{ squares : Array(9).fill(null)}]);
   const [xIsNext, setXIsNext] = useState(true);
-  
+  const [stepNumber, setStepNumber] = useState(0);
   
   const calculateWinner = (squares) => {
     const lines = [
@@ -28,7 +28,7 @@ const App = () => {
     return null;
   }
   
-  const current = history[history.length - 1];
+  const current = history[stepNumber];
   const winner = calculateWinner(current.squares);
   
   let status ;
@@ -39,7 +39,9 @@ const App = () => {
   }
   
   const handleClick = (i) => {
-    const newSquares = current.squares.slice();
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrent = newHistory[newHistory - 1];
+    const newSquares = newCurrent.squares.slice();
     
     // 승자가 이미 나온 경우 경기 중지(리턴), 혹은 현재 클릭한 스퀘어 안에 이미 값이 있다면 그냥 리턴
     if(calculateWinner(newSquares) || newSquares[i]){
@@ -47,12 +49,28 @@ const App = () => {
     }
     
     newSquares[i] = xIsNext ? 'X' : 'O' ;
-    setHistory([...history, {squares: newSquares}]);
+    setHistory([...newHistory, {squares: newSquares}]);
     // setXIsNext(!xIsNext);
     // 위와 같은 코드이나 약간의 차이가 있다. 위처럼 하면 두 번 반복해도 한 번 동작하고, prev를 이용하면 두 번이 따로 다 동작한다.
     setXIsNext(prev => !prev);
+    
+    setStepNumber(newHistory.length);
   }
   
+  const moves = history.map((step, move) => {
+    const desc = move ? 'Go to move #' + move :
+      'Go to game start!';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    )
+  })
+  
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext((step % 2) ===0 );
+  }
   
   return (
     <div className="game">
@@ -61,6 +79,7 @@ const App = () => {
       </div>
       <div className="game-info">
         <div className="status">{status}</div>
+        <ol>{moves}</ol>
     </div>
   </div>
   );
